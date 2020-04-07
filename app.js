7d6964598d6890eef5b275a35e15c016e6690925
ArturRushanyan <rushanyan-artur@mail.ru>
2020-04-07 13:04:57 +0400

    connecting to mongodb

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import config from './config';
 
 const app = express();
 
-
+mongoose.connect(config.DB.url, {
+    useNewUrlParser: true,
+}).then(() => {
+    console.log('Successfully connected to the database')
+}).catch((err) => {
+    console.log('Could not connect to the database. Exiting now...');
+    console.log('err ', err);
+    process.exit();
+});
 app.listen(config.port, () => {
     console.log(`Server is up on ${config.port} port`);
 });
--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 const config = {
     port: 3000,
+    DB: {
+        url: 'mongodb://localhost:27017/alternative_slack',
+    }
 };
 
 module.exports = config;
